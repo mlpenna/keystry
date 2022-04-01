6d082550b7d7ef43adf8b58fc7fe97846d512edd
mlpenna <matheus.lanza.penna@outlook.com>
2022-04-01 13:34:50 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 function App() {
+
+  const [keyPressed, setKeyPressed] = useState("_");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Stack
+          spacing="10"
+          sx={{
+            mt: 10,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="h1" component="h1">
+            Keyboard Speed
+          </Typography>
+          <Typography variant="body" component="h1">
+            {keyPressed}
+          </Typography>
+          <Keyboard
+            physicalKeyboardHighlight={true}
+            physicalKeyboardHighlightTextColor={"black"}
+            layout={{
+              default: [
+                // "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+                "q w e r t y u i o p",
+                "a s d f g h j k l",
+                "z x c v b n m , .",
+                "{space}",
+              ],
+            }}
+            display={{
+              "{_}": " ",
+              "{bksp}": "delete",
+              "{enter}": "enter",
+              "{shift}": "shift",
+              "{space}": " ",
+            }}
+            // onChange={(input) => onChange(input)}
+            // onKeyPress={(button) => onKeyPress(button)}
+            onKeyPress={console.log("asas")}
+            onChange={(input) => console.log(input)}
+          />
+        </Stack>
+      </Box>
+    </Container>
   );
 }
 
